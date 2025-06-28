034ea1533237f7752ad46ddb832d8ca397b5a4f0
menoo-o <menosuper6@gmail.com>
2025-06-28 14:39:59 +0500

    creating navbar pt2.1

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 // import Component from "@/components/construction-page";
 // import ContactButton from "@/components/Contact-Btn/ContactButton";
 // import Contact from "@/components/Contact-form/Contact";
-import Logo from '@/public/logo.svg';
-import Image from 'next/image';
-import Navbar from "@/components/Mini-Nav/Navbar";
-import ContactButton from '@/components/Contact-Btn/ContactButton';
+// import Logo from '@/public/logo.svg';
+// import Image from 'next/image';
+// import Navbar from "@/components/Mini-Nav/Navbar";
+// import ContactButton from '@/components/Contact-Btn/ContactButton';
 
 export default function App() {
   return (
