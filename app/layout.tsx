f9a3af04bf1dfa0187babfb775f16de0a9335921
menoo-o <menosuper6@gmail.com>
2025-10-10 04:20:23 +0500

    structural changes pt7.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import NavOverlay from "../components/Nav-Overlay/NavOverlay";
 import ContactOverlay from "../components/ContactOverlay/Contact-form";
 import Script from "next/script";
 import './globals.css'
-import ParallaxFooter from "@/components/footer-block/footer";
+// import ParallaxFooter from "@/components/footer-block/footer";
 
 
 export const metadata: Metadata = {
